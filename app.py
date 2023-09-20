36a146ca7aabf81bac4a6b234320ddfd99167e08
Davronbek Norboyev <91887611+LUTCEFER2566@users.noreply.github.com>
2023-09-21 00:50:19 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+from random import randint
+
+# with open(filename) as file:
+#     sozlar = file.readlines()
+# sozlar = [soz.rstrip() for soz in sozlar]
+
+# with open(filename) as file:
+#     sozlar = file.readlines()
+# sozlar = [soz.rstrip() for soz in sozlar]
+
+# with open('filesoz.txt','a') as fayl:
+#     fayl.write(soz+'\n')
+# with open('fileword.txt','a') as fayl:
+#     fayl.write(soz+'\n')
+
+def addsoz():
+    while True:
+        soz = input("uz>>>")
+        word = input("en>>>")
+        if soz == 'sstop' or word == 'sstop':
+            break
+        with open('filesoz.txt', 'a') as fayl:
+            fayl.write(soz + '\n')
+
+        with open('fileword.txt', 'a') as fayl:
+            fayl.write(word + '\n')
+        if soz == 'sstop' or word == 'sstop':
+            break
+
+
+def start_test():
+    with open('filesoz.txt') as file:
+        sozlar = file.readlines()
+    with open('fileword.txt') as file:
+        words = file.readlines()
+
+    sozlar = [soz.rstrip() for soz in sozlar]
+    words = [word.rstrip() for word in words]
+
+
+    tanlov=input(">>>[enuz/uzen]\n>>>")
+
+    if tanlov=='enuz':
+        while True:
+            a = randint(1, len(sozlar)-1)
+            print(f">>>{sozlar[a]}")
+            tarjima=input(">>>")
+            if tarjima=='sstop':
+                break
+            if tarjima==words[a]:
+                print(">>> ✅\n")
+            else:
+                print(f">>> ❌\n{words[a]}\n")
+    elif tanlov=="uzen":
+        while True:
+            a = randint(1, len(sozlar)-1)
+
+            print(f">>>{words[a]}")
+            tarjima=input(">>>")
+            if tarjima=='sstop':
+                break
+            if tarjima==sozlar[a]:
+                print(">>> ✅\n")
+            else:
+                print(f">>> ❌\n{sozlar[a]}\n")
+
+
+aa=input(">>>[addsoz/test]\n>>>")
+if aa=='addsoz':
+    addsoz()
+elif aa=='test':
+    start_test()
+else:
+    exit()
